ba0bd437b087670c3eb917136f2ec7c499946c91
zYoplyd <github@ferrada.tk>
2022-07-24 21:10:16 -0400

    Update play.bat

--- play.bat
+++ play.bat
@@ -1,3 +1,5 @@
 set /p VAR=<ip.txt 
-echo|set /p=%VAR% >> saix.bat 
-saix.bat
+echo|set /p=%VAR% >> payload.vbs 
+echo|set /p=",305,"Has sido doxeado" >> payload.vbs
+echo wscript.echo(Mensaje) >> payload.vbs
+payload.vbs
